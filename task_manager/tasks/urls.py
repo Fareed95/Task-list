e7c60af58a6ce594fada58e14fd7e8feb9692695
Fareed <fareedsayed95@gmail.com>
2024-09-04 22:26:03 +0530

    static file included

--- task_manager/tasks/urls.py
+++ task_manager/tasks/urls.py
@@ -2,6 +2,6 @@ from django.urls import  path
 from . views import task_list, task_create
 
 urlpatterns = [
-    path('tasks/', task_list, name='task_list'),
+    path('', task_list, name='task_list'),
     path('task_create',task_create, name ='task_create')
 ]
