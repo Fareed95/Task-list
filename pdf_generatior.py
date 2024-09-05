05b387075d199638463b6f6d71139b867a26d477
Fareed <fareedsayed95@gmail.com>
2024-09-05 18:14:17 +0530

    static file url is set

--- pdf_generatior.py
+++ pdf_generatior.py
@@ -8,9 +8,10 @@ pdf.set_auto_page_break(auto=True, margin=10)  # Adjust margin as needed
 pdf.add_page()
 
 # Set font
-pdf.set_font("Arial", size=8)
+pdf.set_font("Arial", size=11)
 
 # Title
+pdf.cell(200, 6, txt="", ln=True)
 pdf.cell(200, 6, txt="To,", ln=True)
 pdf.cell(200, 6, txt="The Municipal Commissioner,", ln=True)
 pdf.cell(200, 6, txt="Municipal Corporation of Greater Mumbai,", ln=True)
@@ -25,23 +26,22 @@ letter_body = (
     "Subject: Request for Issuance of Birth Certificate\n\n"
     "Respected Sir/Madam,\n\n"
     "I, Mehirun Sayed, daughter of Mustafa Sayed and Sahib bi Khatun Mustafa Sayed, "
-    "am writing to request the issuance of my birth certificate. I was born at BARC Colony, "
-    "Anushakti Nagar, Godavari Building, Room No. 69, First Floor, 400094.  "
-    "Currently, I reside at Room no. 89, Behind Building No.5, Subhash Nagar, Chembur, Mumbai, Maharashtra, 400071. "
-    "After my marriage, I shifted to my present address. My husband's name is Khalil Sayed.\n"
+    "am writing to request the issuance of my birth certificate. I was born on 15th August 1975 at BARC Colony, "
+    "Anushakti Nagar, Godavari Building, Room No. 69, First Floor, 400094. \n\n"
     "In support of my request, I have attached the following documents:\n"
-    "1. Living Certificate\n"
+    "1. Leaving Certificate\n"
     "2. Death Certificate of my father, Mustafa Sayed\n"
     "3. My mother's Aadhar Card and CHS Card xerox\n\n"
     "The birth certificate is mandatory for updating my legal documents such as Aadhar, PAN card, and other related documents.\n"
     "I kindly request you to process my application at the earliest and provide me with my birth certificate."
     "Thank you for your attention to this matter.\n\n"
-    "Yours faithfully,\n"
-    "Mehirun Sayed\n"
+    "Yours faithfully,\n\n\n"
+    "Mehirun Sayed,\n"
+    "W/0 Mr. Khalil Sayed, \n"
     "Room no. 89, Behind Building No.5,\n"
     "Subhash Nagar, Chembur,\n"
     "Mumbai, Maharashtra, 400071\n"
-    "Phone: 9136856391\n"
+    "Phone: 9136856391/9221963391\n"
     "Email: mehirunsayed@gmail.com"
 )
 
@@ -49,7 +49,7 @@ letter_body = (
 pdf.multi_cell(0, 6, letter_body)  # Adjust height parameter to minimize extra space
 
 # Save the PDF to a file
-pdf_file_path = 'C:\\Users\\Admin\\Documents\\Request_for_Birth_Certificate_Full_Letter_final.pdf'
+pdf_file_path = 'C:\\Users\\Admin\\Documents\\Request_for_Birth_Certificate_Full_Letter.pdf'
 
 pdf.output(pdf_file_path)
 
--- task_manager/tasks/urls.py
+++ task_manager/tasks/urls.py
@@ -1,7 +1,10 @@
 from django.urls import  path
 from . views import task_list, task_create
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
 urlpatterns = [
     path('', task_list, name='task_list'),
     path('task_create',task_create, name ='task_create')
 ]
+
+urlpatterns += staticfiles_urlpatterns()
